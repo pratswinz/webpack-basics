f4ea28f34fbecde59c9a26fce7c5a4a340355af7
prateek <prateek.srivastava@redBus.com>
2019-08-27 23:57:05 +0530

    adding terser plugin to minify the bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports ={
     entry:'./src/index.js',
@@ -19,5 +20,8 @@ module.exports ={
                 use:['style-loader','css-loader']
             }
         ]
-    }
+    },
+    plugins:[
+        new TerserPlugin()
+    ]
 }
